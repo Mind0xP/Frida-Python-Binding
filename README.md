b11cc3b573658ad56c976ef53f4a1e4bb8c662df
Mind0xP <guyarazi@gmail.com>
2018-04-17 16:32:54 +0300

    updates

--- README.md
+++ README.md
@@ -2,3 +2,9 @@
 Easy to use Frida python binding script for Android reversing automation.
 
 # So whats in it ?
+![app-demo](https://i.imgur.com/SyNcaix.mp4)
+
+# How-to install
+Make sure to use Python 3.x to run the script.
+install the dependecies by running:
+`pip install -r requirements.txt`
--- pythonBinding.py
+++ pythonBinding.py
@@ -1,10 +1,11 @@
 import frida
-import argparse
-import time
 import os
 import logging
-import sys
 import subprocess
+from termcolor import colored
+from argparse import ArgumentParser
+from time import sleep
+from sys import exit
 from colorlog import ColoredFormatter
 
 def setup_logging():
@@ -39,7 +40,7 @@ def on_message(message, data):
 
 # start frida-server process
 def frida_server_start_process(device_id,frida_server):
-    frida_server_subprocess = subprocess.Popen("adb -s "+device_id+" shell "+frida_server+" &", shell=True)
+    frida_server_subprocess = subprocess.Popen("adb -s "+device_id+" shell "+frida_server+" &", shell=False)
     if frida_server_subprocess:
         return frida_server_subprocess
     else:
@@ -56,14 +57,14 @@ def frida_server_install(device_id, frida_install_path, device_install_path='/da
     try:
         frida_server_device_path = device_install_path+frida_server_name
         frida_push_device = subprocess.Popen("adb -s "+device_id+" push \""+frida_install_path+"\" "+
-                                            frida_server_device_path, shell=True, stderr=subprocess.STDOUT,
+                                            frida_server_device_path, shell=False, stderr=subprocess.STDOUT,
                                             stdout=subprocess.PIPE)
         data, nothing = frida_push_device.communicate()
         data_str = str(data,'utf-8')
         if data_str.index('pushed.'):
             logger.info("Frida-server was successfully pushed to selected device on "+ device_install_path)
             # set frida-server permissions
-            frida_server_perm = subprocess.Popen("adb -s "+device_id+" shell chmod 755 \""+frida_server_device_path+"\"", shell=True,
+            frida_server_perm = subprocess.Popen("adb -s "+device_id+" shell chmod 755 \""+frida_server_device_path+"\"", shell=False,
                                                 stderr=subprocess.STDOUT, stdout=subprocess.PIPE)
             logger.info("Set frida-server with execution permissions")
             frida_server_perm.wait()
@@ -78,7 +79,7 @@ def frida_server_install(device_id, frida_install_path, device_install_path='/da
 # check if frida-server exists on selected device
 def frida_server_check_existing(device_id):
     frida_existing = subprocess.Popen("adb -s "+device_id+" shell \"ps| grep frida-server\"", 
-                                       shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+                                       shell=False, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
     data, nothing = frida_existing.communicate()
     if (len(data) != 0):
         frida_existing.kill()
@@ -118,33 +119,49 @@ def enumerate_connected_devices():
 
 def frida_session_activity_handler(con_device,activity,**kwargs):
     for _ in range(5):
-        if (activity != 'list_pids'):
-            time.sleep(1)
-            if activity == 'pid':
-                frida_session = con_device.attach(int(kwargs.get('pid')))
-            elif activity == 'spawn':
-                pid = con_device.spawn([kwargs.get('spawn')])
-                if pid:
-                    frida_session = con_device.attach(pid)
-                    # resume app after spawning
-                    con_device.resume(pid)
-                    return frida_session
-                else:
-                    pass
+        sleep(1)
+        if activity == 'pid':
+            frida_session = con_device.attach(int(kwargs.get('pid')))
+            if frida_session:
+                logger.info("Attaching frida session to PID - {0}".format(frida_session._impl.pid))
             else:
-                frida_session = con_device.attach(kwargs.get('attach'))
-            if (frida_session):
-                return frida_session
+                logger.error("Could not attach the requested process")
+        elif activity == 'spawn':
+            pid = con_device.spawn([kwargs.get('spawn')])
+            if pid:
+                frida_session = con_device.attach(pid)
+                logger.info("Spawned package : {0} on pid {1}".format(kwargs.get('spawn'),frida_session._impl.pid))
+                # resume app after spawning
+                con_device.resume(pid)
             else:
+                logger.error("Could not spawn the requested package")
                 return None
-        # attach frida to existing running package by name
-        elif (activity == 'list_pids'):
-            time.sleep(1)
+        elif activity == 'attach':
+            frida_session = con_device.attach(kwargs.get('attach'))
+            if frida_session:
+                logger.info("Attaching frida session to process name - {0}".format(kwargs.get('attach')))
+            else: 
+                logger.error("Could not attach to the requested package")
+        else:
+            return None
+        return frida_session
+
+def frida_enumerate_device(con_device,activity):
+    # list pids on selected device
+        if (activity == 'list_pids'):
             pid_list = con_device.enumerate_processes()
             if pid_list:
                 return pid_list
             else:
                 return None
+        # list installed packages on selected device
+        elif (activity == 'list_apps'):
+            apps_list = con_device.enumerate_applications()
+            if apps_list:
+                return apps_list
+            else:
+                return None
+            
 
 # read script file from local FS
 def read_hooking_script(filename):
@@ -157,7 +174,7 @@ def read_hooking_script(filename):
 if __name__ == '__main__':
     try:
         # define application argument parser
-        parser = argparse.ArgumentParser(description="Frida RE Easy Python Binding")
+        parser = ArgumentParser(description="Frida RE Easy Python Binding")
         parser.add_argument("-s","--spawn",help="Set package name to spawning a new process")
         parser.add_argument("-p","--pid",help="Specify a PID number")
         parser.add_argument("-a","--attach",help="Set a process name to attach")
@@ -184,8 +201,8 @@ if __name__ == '__main__':
         if devices:
             selected_device = select_device(devices)
         else:
-            logger.error("No connected devices found")
-            sys.exit(1)
+            logger.error("Could not find any connected devices on your machine")
+            exit(1)
         if frida_server_check_existing(selected_device):
             logger.info("Frida-server is running on the selected device!")
         else:
@@ -195,47 +212,66 @@ if __name__ == '__main__':
                 logger.info("New frida-server process initiated on PID {0}".format(frida_server_process.pid))
             else:
                 logger.error("Frida-server could not be deployed on the selected device!")
-                sys.exit(1)
+                exit(1)
         connected_device = frida.get_device(selected_device, args.timeout)
-        if args.list_pids or args.list_apps:
-            pid_list = frida_session_activity_handler(connected_device,'list_pids')
+        if args.list_pids:
+            pid_list = frida_enumerate_device(connected_device,'list_pids')
             if pid_list:
-                print("-" * 50 + "\n[*] Generated process list on selected device\n" + "-" * 50)
+                print("-" * 50 + "\n[*] Generated running processes list on selected device\n" + "-" * 50)
                 print("Process Name" + " " * 21 + "PID"+"\n"+"-" * 30 + " " + "-" * 7)
                 for key, val in enumerate(pid_list):
-                    spaces = " " * (33 - len(pid_list[key].name))
-                    print("{0}{1}{2}".format(pid_list[key].name,spaces,pid_list[key].pid))
+                    spaces = " " * (33 - len(val.name))
+                    print("{0}{1}{2}".format(val.name,spaces,val.pid))
                 pid_choice = int(input("\n[*] Please enter PID number: "))
                 frida_session = frida_session_activity_handler(connected_device,'pid',pid=pid_choice)
             else:
-                logger.error("Could not enumerate process on the selected device")
-                sys.exit(1)
-        # spawn a new process instance and attac
+                logger.error("Could not enumerate processes on the selected device")
+                exit(1)
+        if args.list_apps:
+            apps_list = frida_enumerate_device(connected_device,'list_apps')
+            if apps_list:
+                print("-" * 50 + "\n[*] Generated installed packages list on selected device\n" + "-" * 50)
+                print("Id" + " " * 10    + "Package Name" + " " * 33 + "Package Identifier"+" " * 27 + "PID" + "\n" + "-" * 110)
+                for key, val in enumerate(apps_list):
+                    if val.pid == 0:
+                        package_pid = 'Not running'
+                    else:
+                        fixed_spaces = 4 * " "
+                        package_pid = fixed_spaces + str(val.pid)
+                    first_colm_spaces = " " * (4 - len(str(key)))
+                    second_colm_spaces = " " * (35 - len(val.name))
+                    third_colm_spaces = " " * (50 - len(val.identifier))
+                    if val.pid == 0:
+                        print("[{0}]{1}|  {2}{3}|  {4}{5}| {6}".format(key,first_colm_spaces,val.name,second_colm_spaces,val.identifier,third_colm_spaces,package_pid))
+                    else:
+                        print(colored("[{0}]{1}|  {2}{3}|  {4}{5}| {6}".format(key,first_colm_spaces,val.name,second_colm_spaces,val.identifier,third_colm_spaces,package_pid),"green"))
+                app_choice = int(input("\n[*] Please enter an id: "))
+                app_choice = apps_list[app_choice].identifier
+                while True:
+                    user_activity_choice = input("\n[?] What do you want to spawn or attach [S/A]?")
+                    if user_activity_choice == 'S':
+                        frida_session = frida_session_activity_handler(connected_device,'spawn',spawn=app_choice)
+                        break
+                    elif user_activity_choice == 'A':
+                        frida_session = frida_session_activity_handler(connected_device,'attach',attach=app_choice)
+                        break
+                    else:
+                        logger.error("Please choose between A (Attach) or S (Spawn)")
+            else:
+                logger.error("Could not enumerate installed packages on the selected device")
+                exit(1)
+        # spawn a new process instance and attach
         elif args.spawn:
             frida_session = frida_session_activity_handler(connected_device,'spawn',spawn=args.spawn)
-            if frida_session:
-                logger.info("Spawned package : {0} on pid {1}".format(args.spawn,frida_session._impl.pid))
-            else:
-                logger.error("Could not spawn the requested package")
-                sys.exit(1)
         # attach frida to existing running pid
         elif args.pid:
             frida_session = frida_session_activity_handler(connected_device,'pid',pid=args.pid)
-            if frida_session:
-                logger.info("Attaching frida session to PID - {0}".format(frida_session._impl.pid))
-            else:
-                logger.error("Could not attach the requested process")
-                sys.exit(1)
         elif args.attach:
-            frida_session = frida_session_activity_handler(connected_device,'attach',attach=args.attach)
-            if frida_session:
-                logger.info("Attaching frida session to process name - {0}".format(args.attach))
-            else: 
-                logger.error("Could not attach to the requested package")
+            frida_session = frida_session_activity_handler(connected_device,'attach',attach=args.attach)                
         else:
             logger.warning("You must specify atleast one option to run the application\n[*] Please use --help for full reference")
         # set timeout so java.perform will not crash
-        time.sleep(1)
+        sleep(1)
 
         # load hooking script
         hooking_script = frida_session.create_script(read_hooking_script(args.script))
